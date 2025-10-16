ec8c7a6f3b30cd9e641c56357944d9d7c8a6a933
melyon976 <melissa.lyon@student.uts.edu.au>
2025-10-16 15:21:53 +1100

    Added two pages, LogIn and Setup Worked on mainly the SignUp page, adding feilds.
    But I will need to link it to the cloud database later.

---
+++ Dementia App Prototype/LogIn.swift
@@ -0,0 +1,18 @@
+//
+//  Login.swift
+//  Dementia App Prototype
+//
+//  Created by Melissa Lyon on 16/10/2025.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Login()
+}
---
+++ Dementia App Prototype/SignUp.swift
@@ -0,0 +1,18 @@
+//
+//  SignUp.swift
+//  Dementia App Prototype
+//
+//  Created by Melissa Lyon on 16/10/2025.
+//
+
+import SwiftUI
+
+struct SignUp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignUp()
+}
